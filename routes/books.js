1fd8f3895bccd6d987212c6de90acd37d0d0dda3
Asfand Yar <asfandyar687@gmail.com>
2024-08-05 16:21:38 +0500

    feat: added book routes

---
+++ routes/books.js
@@ -0,0 +1,175 @@
+const express = require('express');
+const router = express.Router();
+
+// Mock database
+let books = [];
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The title of the book
+ *         author:
+ *           type: string
+ *           description: The author of the book
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Returns the list of all the books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: The list of the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
+router.get('/', (req, res) => {
+  res.json(books);
+});
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       201:
+ *         description: The book was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ */
+router.post('/', (req, res) => {
+  const book = { id: books.length + 1, ...req.body };
+  books.push(book);
+  res.status(201).json(book);
+});
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   get:
+ *     summary: Get the book by id
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *     responses:
+ *       200:
+ *         description: The book description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       404:
+ *         description: The book was not found
+ */
+router.get('/:id', (req, res) => {
+  const book = books.find(b => b.id == req.params.id);
+  if (!book) return res.status(404).send('Book not found');
+  res.json(book);
+});
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   put:
+ *     summary: Update the book by the id
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The book was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       404:
+ *         description: The book was not found
+ */
+router.put('/:id', (req, res) => {
+  const book = books.find(b => b.id == req.params.id);
+  if (!book) return res.status(404).send('Book not found');
+
+  book.title = req.body.title;
+  book.author = req.body.author;
+  res.json(book);
+});
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   delete:
+ *     summary: Remove the book by id
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *     responses:
+ *       200:
+ *         description: The book was deleted
+ *       404:
+ *         description: The book was not found
+ */
+router.delete('/:id', (req, res) => {
+  const bookIndex = books.findIndex(b => b.id == req.params.id);
+  if (bookIndex === -1) return res.status(404).send('Book not found');
+
+  books.splice(bookIndex, 1);
+  res.status(200).send('Book deleted');
+});
+
+module.exports = router;
